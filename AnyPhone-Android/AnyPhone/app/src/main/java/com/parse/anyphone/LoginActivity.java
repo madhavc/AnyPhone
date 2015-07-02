5d94b69a5ccd7cea469bd2c5a34b9eb2e198ebc7
Madhav Chhura <chhura.madhav@gmail.com>
2015-07-02 11:04:29 -0700

    Working login commit

--- AnyPhone-Android/AnyPhone/app/src/main/java/com/parse/anyphone/LoginActivity.java
+++ AnyPhone-Android/AnyPhone/app/src/main/java/com/parse/anyphone/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.parse.anyphone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -8,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,36 +19,63 @@ import com.parse.ParseCloud;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 
 
 public class LoginActivity extends AppCompatActivity {
-   
-    private TextView questionLabel, subtituteLabel;
+
+    private TextView questionLabel, substituteLabel;
     private EditText textField;
     private Button sendCodeButton;
 
     private String phoneNumber = null;
     private String token = null;
     private int code = 0;
-    
+    private int flag = 0; //If flag = 0 call the sendCode method, otherwise call the doLogin method.
+
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        progressBar= (ProgressBar) findViewById(R.id.loadingSpinner);
         questionLabel = (TextView) findViewById(R.id.questionLabel);
-
-        subtituteLabel = (TextView) findViewById(R.id.subtitleLabel);
-
+        substituteLabel = (TextView) findViewById(R.id.subtitleLabel);
         textField = (EditText) findViewById(R.id.phoneNumberField);
-
         sendCodeButton = (Button) findViewById(R.id.sendCodeButton);
 
+        sendCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCodeButton.setClickable(false);
+                if(flag == 0)
+                    sendCode();
+                else
+                    doLogin();
+            }
+        });
         phoneNumberUI();
+    }
+
+    private void phoneNumberUI() {
+        flag = 0;
+        questionLabel.setText("Please enter your phone number to log in:");
+        substituteLabel.setText("This example is limited to 10-digit US numbers");
+        textField.setHint(R.string.number_default);
+        sendCodeButton.setClickable(true);
+
+    }
+
+    private void codeUI(){
+        flag = 1;
+        questionLabel.setText("Enter the 4-digit confirmation code:");
+        substituteLabel.setText("It was sent in an SMS message to +1" + phoneNumber);
+        textField.setText("");
+        textField.setHint("1234");
+        sendCodeButton.setClickable(true);
     }
 
     private void sendCode() {
@@ -62,9 +91,10 @@ public class LoginActivity extends AppCompatActivity {
             HashMap<String, Object> params = new HashMap<String, Object>();
             params.put("phoneNumber", phoneNumber);
             params.put("language", "en");
-
+            progressBar.setVisibility(View.VISIBLE);
             ParseCloud.callFunctionInBackground("sendCode", params, new FunctionCallback<JSONObject>() {
                 public void done(JSONObject response, ParseException e) {
+                    progressBar.setVisibility(View.GONE);
                     if (e == null) {
                         Log.d("Cloud Response", "There were no exceptions! " + response.toString());
                         codeUI();
@@ -80,65 +110,34 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-
-
-    private void phoneNumberUI() {
-        questionLabel.setText("Please enter your phone number to log in:");
-        subtituteLabel.setText("This example is limited to 10-digit US numbers");
-        textField.setHint(R.string.number_default);
-        sendCodeButton.setClickable(true);
-        sendCodeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendCodeButton.setClickable(false);
-                sendCode();
-            }
-        });
-    }
-
-    private void codeUI(){
-        questionLabel.setText("Enter the 4-digit confirmation code:");
-        subtituteLabel.setText("It was sent in an SMS message to +1" + phoneNumber);
-        textField.setText("");
-        textField.setHint("1234");
-        sendCodeButton.setClickable(true);
-        sendCodeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendCodeButton.setClickable(false);
-                doLogin();
-            }
-        });
-    }
-
-
     private void doLogin() {
-        code = Integer.parseInt(textField.getText().toString());
-        if(code != 4){
+        if(textField.getText().toString().length() != 4) {
             Toast.makeText(getApplicationContext(),
                     "You must enter the 4 digit code texted to your phone number.",
                     Toast.LENGTH_LONG).show();
             codeUI();
-        }
-        else{
+        } else {
+            code = Integer.parseInt(textField.getText().toString());
             HashMap<String, Object> params = new HashMap<String, Object>();
             params.put("phoneNumber", phoneNumber);
             params.put("codeEntry", code);
 
-            ParseCloud.callFunctionInBackground("logIn", params, new FunctionCallback<JSONObject>() {
-                public void done(JSONObject response, ParseException e) {
+            progressBar.setVisibility(View.VISIBLE);
+            ParseCloud.callFunctionInBackground("logIn", params, new FunctionCallback<String>() {
+                public void done(String response, ParseException e) {
+                    progressBar.setVisibility(View.GONE);
                     if (e == null) {
-                        try {
-                            token = (String) response.get("token");
-                        } catch (JSONException e1) {
-                            e1.printStackTrace();
-                        }
+                        token = response;
                         Log.d("Cloud Response", "There were no exceptions! " + response);
                         ParseUser.becomeInBackground(token, new LogInCallback() {
                             @Override
                             public void done(ParseUser parseUser, ParseException e) {
-                                if (e == null)
+                                if (e == null){
                                     Log.d("Cloud Response", "There were no exceptions! ");
+                                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(i);
+                                    finish();
+                                }
                                 else {
                                     Log.d("Cloud Response", "Exception: " + e);
                                     Toast.makeText(getApplicationContext(),
