a22ebb6a80a2e4f3e2d0511c3d59323c27236cfa
Madhav Chhura <chhura.madhav@gmail.com>
2015-07-02 14:24:45 -0700

    Remove personal keys

--- AnyPhone-Android/AnyPhone/app/src/main/java/com/parse/anyphone/Application.java
+++ AnyPhone-Android/AnyPhone/app/src/main/java/com/parse/anyphone/Application.java
@@ -8,21 +8,7 @@ import com.parse.Parse;
 public class Application extends android.app.Application {
     public void onCreate(){
 
-        // Enable Local Datastore.
-        //Parse.enableLocalDatastore(this);
-
-        Parse.initialize(this, "xuOtNShZhO5haAKndUgLZNkXBsmsMngXfqWzEnqT", "rxDmDXCKUK0zGOfLaNJdJiX7kF5OxsIjvnyhUdF6");
-
-//        //Code below is used for Parse Notifications.
-//        ParsePush.subscribeInBackground("", new SaveCallback() {
-//            @Override
-//            public void done(ParseException e) {
-//                if (e == null) {
-//                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
-//                } else {
-//                    Log.e("com.parse.push", "failed to subscribe for push", e);
-//                }
-//            }
-//        });
+        //Add below your Parse project application and client keys.
+        Parse.initialize(this, "ParseApplicationID", "ParseClientKey");
     }
 }
