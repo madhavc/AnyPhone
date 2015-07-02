53d1f6e80730ad810c9e8482f5cffe55273a9881
Madhav Chhura <chhura.madhav@gmail.com>
2015-07-02 14:52:34 -0700

    Update the SplashActivity

--- AnyPhone-Android/AnyPhone/app/src/main/java/com/parse/anyphone/SplashActivity.java
+++ AnyPhone-Android/AnyPhone/app/src/main/java/com/parse/anyphone/SplashActivity.java
@@ -2,6 +2,7 @@ package com.parse.anyphone;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,22 +11,37 @@ import com.parse.ParseUser;
 
 
 public class SplashActivity extends AppCompatActivity {
-
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
 
-        if(ParseUser.getCurrentUser() != null){
-            Intent i = new Intent(SplashActivity.this, MainActivity.class);
-            startActivity(i);
-            finish();
-        }
-        else{
-            Intent i = new Intent(SplashActivity.this, LoginActivity.class);
-            startActivity(i);
-            finish();
-        }
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                if(ParseUser.getCurrentUser() != null){
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+                else{
+                    Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        }, SPLASH_TIME_OUT);
+
+
     }
 
     @Override
